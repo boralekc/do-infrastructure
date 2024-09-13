b830c6d8e5d60180675c66320ec201e281f7ec9c
boralekc <boralekc@gmail.com>
2024-09-13 16:50:39 +0200

    34

--- terraform/modules/spaces/main.tf
+++ terraform/modules/spaces/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
 resource "digitalocean_spaces_bucket" "s3-deutschhub" {
   name   = var.space_dutschhub
   region = var.region
